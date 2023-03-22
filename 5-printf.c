9e8c0aa70f7a2e22a2a70b7bcd49a770dd8c30a5
Lerato37 <lee@gmail.com>
2023-03-22 01:25:42 -0700

    6-size.c

---
+++ 5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - A program that print line using printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("With proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
