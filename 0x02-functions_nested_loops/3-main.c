ccb0cfc162fb35419baac1668433cd9c0cd3e156
Lerato37 <lee@gmail.com>
2023-03-24 02:36:59 -0700

    3-islower

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -11,11 +11,11 @@ int main(void)
 	int r;
 
 	r = _islower('H');
-	_putcharhg(r + '0');
+	_putchar(r + '0');
 	r = _islower(0);
 	_putchar(r + '0');
 	r = _islower(108);
 	_putchar(r + '0');
-	_putcha('\n');
+	_putchar('\n');
 	return (0);
 }
